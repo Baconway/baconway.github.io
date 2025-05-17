1aff583fb35b695e9dfecb56dd7c13bfa89b1756
Baconway <155370111+Baconway@users.noreply.github.com>
2025-05-17 23:29:21 +0700

    Added links to 'platform' section of landing page

--- junior scripts/pulldown.js
+++ junior scripts/pulldown.js
@@ -1,31 +1,47 @@
 const childLists = {
-    'Platform' : [
-        'Roblox: Bac0nway',
-        'Youtube: Baconway',
-        'NovelUpdates: Baconway',
+  Platform: {
+    Roblox: ": ",
+    Youtube: ": ",
+    NovelUpdates: ": ",
+    Leetify: " (CS2 Stats) : ",
+    Etterna: ": ",
+    "osu!": ": ",
+  },
 
-    ],
-    'Game' : [
-        'Arknights: Baconway#5628 (inactive)',
-        'maimai: 101820453238481',
-        'Honkai: Star Rail: 802221265 (Asia)',
-        'Azur Lane: Baconway (Avrora server)',
-        'Blue Archive: AYYFRDKZ',
-        'Conflict Of Nations: Baconway',
-    ]
-}
+  Game: {
+    "Arknights: ": "Baconway#5628 (inactive)",
+    "maimai: ": "101820453238481",
+    "Honkai: Star Rail: ": "802221265 (Asia)",
+    "Azur Lane : ": "Baconway (Avrora server)",
+    "Blue Archive:": " AYYFRDKZ",
+    "Conflict Of Nations: ": "Baconway",
+  },
+};
+
+const Links = {
+  "osu!": "https://osu.ppy.sh/users/20144046",
+  Etterna: "https://etternaonline.com/users/Baconway",
+  Youtube: "https://www.youtube.com/channel/UCTgtTyEKojAzULPDsRcBm0A",
+  Leetify: "https://leetify.com/app/profile/76561199216739443",
+  NovelUpdates: "https://www.novelupdates.com/user/710476/Baconway/",
+  Roblox: "https://www.roblox.com/users/243533657/profile",
+};
 
 function buttonPress(parent) {
-    const bulletParent = document.getElementById(parent)
+  const bulletParent = document.getElementById(parent);
 
-    if (bulletParent.getElementsByTagName('li').length <= 0) {
-        childLists[parent].forEach(element => {
-            const bullet = document.createElement('li')
-            bullet.innerHTML = `<span>${element}</span>`
+  if (bulletParent.getElementsByTagName("li").length <= 0) {
+    for (let [key, value] of Object.entries(childLists[parent])) {
+      const bullet = document.createElement("li");
+      bullet.innerHTML = `<span>${key} ${value}</span>`;
 
-            bulletParent.appendChild(bullet)
-        });
-    } else {
-        bulletParent.innerHTML = ''
-    }    
+      if (Links[key]) {
+        bullet.innerHTML += `<a href="${Links[key]}">Link</a>`;
+      }
+
+      bulletParent.appendChild(bullet);
+    }
+  } else {
+    bulletParent.innerHTML = "";
+  }
 }
