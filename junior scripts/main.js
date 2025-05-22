18f47fa6e45a5b77485e15e6640600f12627c861
Baconway <155370111+Baconway@users.noreply.github.com>
2025-05-22 20:35:12 +0700

    Fixed status bar not showing up

--- junior scripts/main.js
+++ junior scripts/main.js
@@ -1,22 +1,33 @@
 //handles site icon, site status bar (direct with <script> in every html file)
 
 const status_colors = {
-    'online' : 'green',
-    'dnd' : 'red'
+  online: "green",
+  dnd: "red",
 };
 
 function setExtras(status, activity, displayTitle) {
-    const statusBar = document.getElementById('status')
-    statusBar.style.backgroundColor = status_colors[status]
-    statusBar.innerText = activity
+  const statusBar = document.getElementById("status");
+  statusBar.style.backgroundColor = status_colors[status];
+  statusBar.innerText = activity;
 
-    const title = document.getElementsByTagName('title')[0]
-    title.innerText = displayTitle
+  const title = document.getElementsByTagName("title")[0];
+  title.innerText = displayTitle;
 }
 
-fetch('https://api.lanyard.rest/v1/users/714482641134551071').then(response => response.json()).then(data => {
-    const av_link = "https://cdn.discordapp.com/avatars/" + data.data.discord_user.id + '/' + data.data.discord_user.avatar + ".png?size=32"
-
-    document.getElementById('icon').href = av_link
-    setExtras(data.data.discord_status, data.data.activities[1].name, data.data.discord_user.display_name)
-})
+fetch("https://api.lanyard.rest/v1/users/714482641134551071")
+  .then((response) => response.json())
+  .then((data) => {
+    const av_link =
+      "https://cdn.discordapp.com/avatars/" +
+      data.data.discord_user.id +
+      "/" +
+      data.data.discord_user.avatar +
+      ".png?size=32";
+    console.log(data);
+    document.getElementById("icon").href = av_link;
+    setExtras(
+      data.data.discord_status,
+      data.data.activities[0].name,
+      data.data.discord_user.display_name
+    );
+  });
