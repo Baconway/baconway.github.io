fb422f3736aba2eae3892e879ad261d387f5585e
Baconway <155370111+Baconway@users.noreply.github.com>
2025-07-15 12:24:07 +0700

    New Activity showcase menu

--- junior scripts/main.js
+++ junior scripts/main.js
@@ -1,42 +1,86 @@
 //handles site icon, site status bar (direct with <script> in every html file)
+let activities;
 
 const status_colors = {
-  online: "green",
-  dnd: "red",
+  online: { color: "green", text: "Online" },
+  dnd: { color: "red", text: "Do Not Disturb" },
 };
 
-function setExtras(status, activity, displayTitle) {
+function interpretImageLinks(name, application_id, img) {
+  if (!img || img == undefined) {
+    return "https://i.sstatic.net/mwFzF.png";
+  }
+  if (img.substring(0, 3) != "mp:") {
+    return `https://cdn.discordapp.com/app-assets/${application_id}/${img.substring(
+      0
+    )}`;
+  } else {
+    return `https://media.discordapp.net/${img.substring(3)}`;
+  }
+}
+
+function receiveTemplate({ application_id, name, details, state, assets }) {
+  let large_img, small_img;
+
+  large_img = interpretImageLinks(name, application_id, assets.large_image);
+  small_img = interpretImageLinks(name, application_id, assets.small_image);
+
+  return `<div class="displayer">
+    Activity
+        <div class="activityInfo">
+          <div class='images'>
+            <img title='${assets.large_text}'
+            src=${large_img} /> 
+            <img class='small' title='${assets.small_text}'
+            src=${small_img} />
+          </div>
+          <div class="activityText">
+            <p id="name" title=${name}>${name}</p>
+            <p id="details" title=${details}>${details}</p>
+            <p id="state" title=${state}>${state}</p>
+          </div>
+        </div>
+      </div>`;
+}
+
+function setExtras(status, displayTitle) {
   const statusBar = document.getElementById("status");
-  statusBar.style.backgroundColor = status_colors[status];
-  statusBar.innerText = activity;
+  statusBar.style.backgroundColor = status_colors[status].color;
+  statusBar.innerText = status_colors[status].text;
 
   const title = document.getElementsByTagName("title")[0];
   title.innerText = displayTitle;
 }
 
+function getActivities() {
+  const dimmer = document.getElementsByClassName("activitiesDisplay")[0];
+
+  if (dimmer.style.visibility != "visible") {
+    dimmer.style.visibility = "visible";
+    if (dimmer.childNodes.length > 0) {
+      return;
+    }
+
+    for (let index = 0; index < activities.length; index++) {
+      const ac = activities[index];
+
+      if (ac.id == "custom") {
+        continue;
+      }
+
+      dimmer.innerHTML += receiveTemplate(ac);
+    }
+  } else {
+    dimmer.style.visibility = "hidden";
+  }
+}
+
 fetch("https://api.lanyard.rest/v1/users/714482641134551071")
   .then((response) => response.json())
   .then((data) => {
-    const av_link =
-      "https://cdn.discordapp.com/avatars/" +
-      data.data.discord_user.id +
-      "/" +
-      data.data.discord_user.avatar +
-      ".png?size=32";
-    console.log(data);
+    const av_link = `https://cdn.discordapp.com/avatars/${data.data.discord_user.id}/${data.data.discord_user.avatar}.png?size=32`;
     document.getElementById("icon").href = av_link;
 
-    if (data.data.activities.length > 0) {
-      setExtras(
-        data.data.discord_status,
-        data.data.activities[0].name,
-        data.data.discord_user.display_name
-      );
-    } else {
-      setExtras(
-        data.data.discord_status,
-        "Idle",
-        data.data.discord_user.display_name
-      );
-    }
+    setExtras(data.data.discord_status, data.data.discord_user.display_name);
+    activities = data.data.activities;
   });
